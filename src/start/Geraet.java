208066845cfaa20becebfc75066d723ec166f639
sy963-cpu <ronorazoro470@gmail.com>
2026-03-25 10:12:38 +0000

    Alle Änderungen committen

--- src/start/Geraet.java
+++ src/start/Geraet.java
@@ -4,41 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Die Klasse Geraet repräsentiert ein physisches Trainingsgerät im Fitnessstudio.
- * Sie verwaltet zudem eine Liste aller Kurse, in denen dieses Gerät genutzt wird.
+ * Die Klasse Geraet reprï¿½sentiert ein physisches Trainingsgerï¿½t im Fitnessstudio.
+ * Sie verwaltet zudem eine Liste aller Kurse, in denen dieses Gerï¿½t genutzt wird.
  */
 public class Geraet {
-    // Der Name des Geräts (z.B. "Hantelbank" oder "Laufband")
+    // Der Name des Gerï¿½ts (z.B. "Hantelbank" oder "Laufband")
     private String name;
     
-    // Eine Liste vom Typ "Kurs", die alle Kurse speichert, die dieses Gerät benötigen.
+    // Eine Liste vom Typ "Kurs", die alle Kurse speichert, die dieses Gerï¿½t benï¿½tigen.
     // Das ist eine sogenannte 1:n oder m:n Beziehung in der Programmierung.
     private List<Kurs> wirdGenutztIn = new ArrayList<>();
 
     /**
-     * Konstruktor: Erstellt ein neues Gerät mit einem Namen.
-     * @param name Der Name des Geräts
+     * Konstruktor: Erstellt ein neues Gerï¿½t mit einem Namen.
+     * @param name Der Name des Gerï¿½ts
      */
     public Geraet(String name) { 
         this.name = name; 
     }
 
     /**
-     * Diese Methode verknüpft das Gerät mit einem Kurs.
+     * Diese Methode verknï¿½pft das Gerï¿½t mit einem Kurs.
      * Sie stellt sicher, dass ein Kurs nicht doppelt in die Liste aufgenommen wird.
-     * * @param k Der Kurs, der hinzugefügt werden soll
+     * * @param k Der Kurs, der hinzugefï¿½gt werden soll
      */
     public void addKurs(Kurs k) { 
-        // Die if-Abfrage prüft, ob der Kurs schon in der Liste ist (! bedeutet "nicht")
+        // Die if-Abfrage prï¿½ft, ob der Kurs schon in der Liste ist (! bedeutet "nicht")
         if (!wirdGenutztIn.contains(k)) {
             wirdGenutztIn.add(k); // Falls nicht vorhanden, ab in die Liste
         }
     }
 
     /**
-     * Eine Getter-Methode, um den Namen des Geräts von außen abzufragen,
+     * Eine Getter-Methode, um den Namen des Gerï¿½ts von auï¿½en abzufragen,
      * da die Variable "name" privat (private) ist.
-     * * @return Den Namen des Geräts
+     * * @return Den Namen des Gerï¿½ts
      */
     public String getName() { 
         return name; 
